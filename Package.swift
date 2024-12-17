f91ad30d77d95b7e9e57ed70afdba6ad1a949c50
pjr.monto@gmail.com <pjr.monto@gmail.com>
2024-12-17 12:30:34 +0100

    changed my mind, no redis

--- Package.swift
+++ Package.swift
@@ -11,8 +11,6 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.99.3"),
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
-        //redis
-        .package(url: "https://github.com/vapor/redis.git", from: "4.0.0"),
     ],
     targets: [
         .executableTarget(
@@ -21,7 +19,6 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
-                .product(name: "Redis", package: "redis"),
             ],
             swiftSettings: swiftSettings
         ),
