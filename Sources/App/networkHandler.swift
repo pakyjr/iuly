dced29687b9bf2059200bca7a95dde91878fd0ac
pjr.monto@gmail.com <pjr.monto@gmail.com>
2024-12-18 16:20:49 +0100

    Implemented NetworkHandler

---
+++ Sources/App/networkHandler.swift
@@ -0,0 +1,31 @@
+import Alamofire
+
+final class NetworkHandler {
+    static let shared = NetworkHandler()
+
+    private init() {}
+
+    /// Generic function to perform an HTTP request
+    func request<T: Decodable>(
+        url: String,
+        method: HTTPMethod = .get,
+        parameters: [String: Any]? = nil,
+        headers: HTTPHeaders? = nil,
+        completion: @escaping (Result<T, Error>) -> Void
+    ) {
+        AF.request(
+            url,
+            method: method,
+            parameters: parameters,
+            encoding: method == .get ? URLEncoding.default : JSONEncoding.default,
+            headers: headers
+        ).validate().responseDecodable(of: T.self) { response in
+            switch response.result {
+            case .success(let decodedData):
+                completion(.success(decodedData))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
